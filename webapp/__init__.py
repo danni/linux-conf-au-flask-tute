d6c8d8f0622f63320b0a43a513e77d128e9f10fc
Danielle Madeley <danielle@madeley.id.au>
2013-12-19 18:51:44 +0000

    Example 2

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@
 WSGI webapp using Flask
 """
 
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 
 
 app = Flask(__name__)
@@ -13,6 +13,20 @@ def index():
     """Homepage"""
 
     return "Hello, linux.conf.au"
+
+
+@app.route('/a-redirect')
+def a_redirect():
+    """Redirect the user"""
+
+    return redirect(url_for('a_template'))
+
+
+@app.route('/a-template')
+def a_template():
+    """Render a template"""
+
+    return render_template('template.html')
 
 
 if __name__ == '__main__':
