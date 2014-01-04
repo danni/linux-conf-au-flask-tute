86cc84f95dc6e4da38b757c8be2e1ba3f30fae4f
Danielle Madeley <danielle@madeley.id.au>
2014-01-04 09:11:09 +0800

    Create user form from model

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,15 +2,18 @@
 WSGI webapp using Flask
 """
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+from flask.ext.wtf import Form
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from wtforms.ext.sqlalchemy.orm import model_form
+
 
 app = Flask(__name__)
 app.secret_key = 'THIS IS REALLY SECRET'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../app.db'
 
 manager = Manager(app)
 
@@ -26,6 +29,9 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), unique=True)
+
+UserForm = model_form(User, base_class=Form)
 
 
 @app.route('/')
@@ -33,6 +39,25 @@ def index():
     """Homepage"""
 
     return "Hello, linux.conf.au"
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    """Register a new user"""
+
+    obj = User()
+    form = UserForm(request.form, obj)
+
+    if form.validate_on_submit():
+        form.populate_obj(obj)
+        db.session.add(obj)
+        db.session.commit()
+
+        return redirect(url_for('register'))
+
+    return render_template('template.html',
+                           form=form,
+                           users=User.query.all())
 
 
 if __name__ == '__main__':
