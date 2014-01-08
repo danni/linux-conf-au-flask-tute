ae12945cee1e6b3f1f7c2ad9344b8da3306a0016
Danielle Madeley <danielle@madeley.id.au>
2014-01-08 14:02:27 +0800

    Add more configuration from openshift

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,10 +10,16 @@ from flask import Flask
 from flask.ext.migrate import Migrate
 from flask.ext.sqlalchemy import SQLAlchemy
 
+try:
+    static_folder = os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
+                                 'wsgi')
+except KeyError:
+    static_folder = None
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=static_folder)
 
-app.secret_key = 'THIS IS REALLY SECRET'
+app.secret_key = os.environ.get('OPENSHIFT_SECRET_TOKEN',
+                                'THIS IS REALLY SECRET')
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL',
                    'sqlite:///../app.db')
