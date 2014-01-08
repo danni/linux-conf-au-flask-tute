3f9b14fa9c0a5caf3ce56efaf32da8a50f5d4d9d
Danielle Madeley <danielle@madeley.id.au>
2014-01-08 15:13:27 +0800

    Configure collectstatic run it on build

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,19 +10,18 @@ from flask import Flask
 from flask.ext.migrate import Migrate
 from flask.ext.sqlalchemy import SQLAlchemy
 
-try:
-    static_folder = os.path.join(os.environ['OPENSHIFT_REPO_DIR'],
-                                 'wsgi')
-except KeyError:
-    static_folder = None
-
-app = Flask(__name__, static_folder=static_folder)
+app = Flask(__name__)
 
 app.secret_key = os.environ.get('OPENSHIFT_SECRET_TOKEN',
                                 'THIS IS REALLY SECRET')
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     os.environ.get('OPENSHIFT_POSTGRESQL_DB_URL',
                    'sqlite:///../app.db')
+
+try:
+    app.static_folder = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi')
+except KeyError:
+    pass
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
